93198166c2b0c760dfc177548bb010022607d9dc
FabianCh <fabianchauvin@gmail.com>
2019-03-28 12:20:44 +0100

    Change on the solver

--- src/Solver.java
+++ src/Solver.java
@@ -1,37 +1,87 @@
-//import java.util.Stack;
-//
-//
-//public class Solver {
-//    private Stack stackHexagon = new Stack();
-//    private Stack stackPentagon = new Stack();
-//    private SoccerBall ball;
-//
-//    private Solver() {
-//        for (int i = 0; i < Data.NB_ELEMENTS.length; i++) {
-//            for (int j = 0; j < Data.NB_ELEMENTS[i]; j++){
-//                if (Data.ELEMENTS_SIDES[i].length == 6) {
-//                    stackHexagon.push(new Polygon(i));
-//                }
-//                else {
-//                    stackPentagon.push(new Polygon(i));
-//                }
-//
-//            }
-//        }
-//    }
-//
-//    public void solve(){
-//
-//    }
-//
-//    public static void main(String[] args){
-//        Solver solver = new Solver();
-//        for (int i = 0; i < solver.pieces_available.length; i++){
-//            System.out.println(solver.pieces_available[i]);
-//        }
-//
-//    }
-//
-//
-//
-//}
+import java.util.ArrayList;
+
+
+public class Solver {
+    private ArrayList<Polygon> listHexagon = new ArrayList<>();
+    private ArrayList<Polygon> listPentagon = new ArrayList<>();
+    private SoccerBall ball;
+
+    private Solver() {
+        for (int i = 0; i < Data.NB_ELEMENTS.length; i++) {
+            for (int j = 0; j < Data.NB_ELEMENTS[i]; j++){
+                if (Data.ELEMENTS_SIDES[i].length == 6) {
+                    listHexagon.add(new Polygon(i));
+                }
+                else {
+                    listPentagon.add(new Polygon(i));
+                }
+            }
+        }
+        ball = new SoccerBall(Data.CONNECTIONS);
+    }
+
+    public void solve()throws Exception{
+        while (!ball.isComplete()) {
+            if (ball.typeFace() == 6) {
+
+                if (!testpolygons(listHexagon)) {
+                    Polygon polygon = ball.popFace();
+                    rangePolygon(polygon);
+                }
+            }
+            else {
+                if (!testpolygons(listPentagon)) {
+                    Polygon polygon = ball.popFace();
+                    rangePolygon(polygon);
+                }
+            }
+        }
+    }
+
+    private boolean testPolygonOrientation(Polygon polygon)throws Exception{
+        polygon.reinitialize();
+        while(!polygon.completeRotation()) {
+            ball.addFace(polygon);
+            if (!ball.hasConflict()) {
+                return true;
+            } else {
+                ball.popFace();
+                polygon.rotate();
+            }
+        }
+        return false;
+    }
+
+    private  boolean testpolygons(ArrayList<Polygon> polygonList) throws Exception{
+        int index = 0;
+        while (index < polygonList.size()) {
+            Polygon polygon = polygonList.get(index);
+
+            if (testPolygonOrientation(polygon)) {
+                listPentagon.remove(index);
+                return true;
+            }
+            else {
+                index++;
+            }
+        }
+        return false;
+    }
+
+    private void rangePolygon(Polygon polygon) {
+        if (polygon.getType() == 6) {
+            listHexagon.add(polygon);
+        }
+        else {
+            listPentagon.add(polygon);
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        }
+
+
+
+}
